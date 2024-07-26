9ad97659fb53b9c63e145bdd59214a0c94e64c69
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2024-07-26 14:23:08 -0700

    Add files via upload
    nested-forLoop.js

---
+++ nested-forLoop.js/nested-forLoop.js
@@ -0,0 +1,75 @@
+// CHAPTER 20: For Loops nested
+
+// ===============================
+// Exercise 0: simple nested loop.
+// =================================
+// Iteration of loop
+// let count = 0;
+
+// // outer loop
+// for(var i = 1; i <= 3; i++){
+//   for(var j = 1; j <= 3; j++){
+//      console.log(`i:${i} j:${j}`);
+//      count++;
+//   }
+// }
+// console.log({count});
+
+// =====================
+// Execercise 1: Multiplication table.
+// =====================
+
+// for(var i = 1; i <= 5; i++){
+//    for(var j = 1; j <= 5; j++ ){
+//      console.log(`${i} * ${j} = ${i * j}`);
+//    }
+//    console.log("===========");
+// }
+
+// =====================
+// Execercise 2: Multiplication table(single number)(user input).
+// =====================
+
+// const userinpt = +prompt("please enter any number to get its table");
+// for(var i = 1; i<=10; i++){
+//     var result = userinpt * i;
+//     console.log(`${userinpt}*${i}=${result}`);    
+// }
+
+// =====================
+// Execercise 3: Repeating characters
+// =====================
+
+// let characters = "ABC";
+
+// for(let i = 0; i < 3; i++){
+//     for(let j = 0; j < characters.length; j++){
+//         console.log(characters[i]);
+//     }
+// }
+
+// =====================
+// Exercise 4 : Triangle pattern with stars
+// =====================
+
+// const rows = 5;
+
+// for(let i = 1; i <= rows; i++){
+// //    for lines rows
+// let stars = "";
+// for (let j = 1; j <= i; j++){
+//     stars += "*";
+   
+// }
+// console.log(stars);
+// }
+
+
+
+
+
+
+
+
+
+
